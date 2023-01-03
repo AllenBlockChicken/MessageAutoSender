a641d83616608359e4ce3dee3e7a7160e2d6f7bc
CreateMiracleStudio <CreateMiracle3330.163.com>
2023-01-03 12:57:55 +0800

    v1.0 update

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import pyautogui
 import random
 import time
+import pygame
+import sys
+
+pygame.init()
+
+mainScreen = pygame.display.set_mode((1280, 700))
+pygame.display.set_caption("Message Auto Sender")
+background = pygame.image.load("./resources/background.png")
+mainScreen.blit(background, (0, 0))
+
+
+class Button(object):
+
+    def __init__(self):
+        pass
+
+    def update(self, imagePath: str, PosX: int, PosY: int):
+        image = pygame.image.load(imagePath)
+        self.PosX = PosX
+        self.PosY = PosY
+        mainScreen.blit(image, (PosX, PosY))
+
+    def onClick(self, mouseX: int, mouseY: int):
+        if self.PosX <= mouseX <= self.PosX + 100 and self.PosY <= mouseY <= self.PosY + 50:
+            return True
+
+        else:
+            return False
 
 
 class Message(object):
@@ -15,7 +43,25 @@ class Message(object):
             pyautogui.typewrite(self.txt)
             pyautogui.press("enter")
 
+message = Message()
 
+button1 = Button()
+button1.update(imagePath="./resources/buttons/button1.png", PosX=50, PosY=50)
 
-sendMessage = Message()
-sendMessage.execute()
+while True:
+    mainScreen.blit(background, (0, 0))
+    button1.update(imagePath="./resources/buttons/button1.png", PosX=50, PosY=50)
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(())
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX = event.pos[0]
+            mouseY = event.pos[1]
+            if button1.onClick(mouseX=mouseX, mouseY=mouseY) == True:
+                message.execute()
+
+            else:
+                print("sorry man")
